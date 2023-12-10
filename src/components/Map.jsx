769d038645bc1dd6f79dba2cff2e7dd0d3c77b92
nizarIng <nizar.ilahi@gmail.com>
2023-12-10 17:11:12 +0100

    Setting Map position with the geolocation feature using a custom hook for that :)

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -4,13 +4,16 @@ import {MapContainer, Marker, Popup, TileLayer, useMap, useMapEvent} from "react
 import {useEffect, useState} from "react";
 import {useCities} from "../contexts/CitiesContext.jsx";
 
+import {useGeolocation} from "../hooks/useGeoLocation.js";
+import {Button} from "./Button.jsx";
+
 import styles from './Map.module.css';
 
 function Map() {
     const [maPosition, setMaPosition] = useState([48.23628429780171, 16.356129201496902]);
     const [searchParams] = useSearchParams();
-
     const {cities} = useCities();
+    const {isLoading: isLoadingPosition, position: geoLocationPosition, getPosition} = useGeolocation();
 
     const mapLat = searchParams.get('lat');
     const mapLng = searchParams.get('lng');
@@ -19,8 +22,18 @@ function Map() {
       if(mapLat && mapLng) setMaPosition([mapLat, mapLng]);
   }, [mapLat, mapLng]);
 
+    useEffect(() => {
+        if(geoLocationPosition){
+            const {lat, lng} = geoLocationPosition;
+            setMaPosition([lat, lng]);
+        }
+    }, [geoLocationPosition]);
+
     return (
         <div className={styles.mapContainer} >
+            {
+              !geoLocationPosition && <Button type="position" onClick={getPosition}>{isLoadingPosition ? 'is loading ...' : 'Use Your Position'}</Button>
+            }
             <MapContainer className={styles.map} center={maPosition} zoom={6} scrollWheelZoom={true}>
                 <TileLayer
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
